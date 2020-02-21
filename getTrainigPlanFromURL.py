ef10384d2c9dbf04766ed78879062793560ce877
MenosGrandes <kaczor201@o2.pl>
2020-02-21 22:46:32 +0100

    Upgraded TrainingPlan to get ics Calendar"

--- getTrainigPlanFromURL.py
+++ getTrainigPlanFromURL.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+import pendulum
+import xlsxwriter
+from ics import Calendar, Event
+
 url = 'https://www.triathlete.com/2019/01/training/7033-triathlon-training-plan_338503'
 r = requests.get(url)
 html_doc = r.text
@@ -9,35 +13,40 @@ article_body =  soup.select_one('.article-body')
 all_p = article_body.find_all('p')
 exercises = []
 previous_week = ""
+date = pendulum.datetime(2020, 1, 27) #27.01.0219
 for p in all_p:
-    day = ""
     week = p.find_previous_sibling('h2')
     span = p.select_one('span')
-    if span is not None:
-        p.span.extract()
-        day = span.select_one('strong').text
+    if span is None:
+        continue
+    p.span.decompose()
     if week is not None:
         if week != previous_week:
-            exercises.append([week.text,day, p.text])
+            exercises.append([week.text,date.format('dddd'), p.text,date.format('YYYY-MM-DD')])
             previous_week = week
         else:
-            exercises.append(["",day, p.text])
+            exercises.append(["",date.format('dddd'), p.text,date.format('YYYY-MM-DD')])
+    date = date.add(days=1)
 
-    print("~!~!~!")
-    
-    
-    
-import xlsxwriter
-
+c = Calendar()
 # Create a workbook and add a worksheet.
 workbook = xlsxwriter.Workbook('TrainingPlan.xlsx')
 worksheet = workbook.add_worksheet()
 row = 0
 col = 0
-for item, cost, p in (exercises):
-    worksheet.write(row, col,     item)
-    worksheet.write(row, col + 1, cost)
-    worksheet.write(row, col + 2, p)
 
+for week, day, name, date in (exercises):
+    worksheet.write(row, col,     week)
+    worksheet.write(row, col + 1, day)
+    worksheet.write(row, col + 2, name)
+    worksheet.write(row, col + 3, date)
     row += 1
+    e = Event()
+    e.name = name
+    e.begin = date
+    e.make_all_day()
+    c.events.add(e)
+
 workbook.close()
+with open('training.ics', 'w') as my_file:
+    my_file.writelines(c)
